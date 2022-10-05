3103479fa69afcdcffd3d6d826d75d200ea2015f
GreedlyCore <naganariselves@gmail.com>
2022-10-05 22:51:11 +0300

    Finished second lab

--- Java/lab2/BattleField.java
+++ Java/lab2/BattleField.java
@@ -7,7 +7,7 @@ import ru.ifmo.se.pokemon.Pokemon;
 public class BattleField {
     public static void main(String[] args) {
         Battle b = new Battle();
-        Pokemon p1 = new Alomomola("Dora", 9);//new Pokemon("Чужой", 1);
+        Pokemon p1 = new Alomomola("Dora", 9);
         Pokemon p2 = new Alomomola("MaybeBaby", 9);
         b.addAlly(p1);
         b.addFoe(p2);
